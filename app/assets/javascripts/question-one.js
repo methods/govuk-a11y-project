acfc06c8d10a62b22f15816c25574e37b526e831
Lakorthus <julio28ve@gmail.com>
2023-05-31 10:52:33 +0100

    readme file and supertest removed

--- app/assets/javascripts/question-one.js
+++ app/assets/javascripts/question-one.js
@@ -6,6 +6,12 @@ const hintBtn = document.querySelector("#fifty-fifty");
 const options = document.querySelectorAll(".govuk-radios__item");
 const option1 = document.querySelector("#question-one-3");
 
+// Add event listener to form submission and 50/50 button
+questionCheck.addEventListener("submit", (event) =>
+  onSubmit(event, isCorrectCheck)
+);
+hintBtn.addEventListener("click", () => removeHalfOptions(hintBtn, options));
+
 function isCorrectCheck() {
   let isCorrect = false;
   if (option1.checked) {
@@ -13,10 +19,3 @@ function isCorrectCheck() {
   }
   return isCorrect;
 }
-
-// Add event listener to form submission and 50/50 button
-questionCheck.addEventListener("submit", (event) =>
-  onSubmit(event, isCorrectCheck)
-);
-
-hintBtn.addEventListener("click", () => removeHalfOptions(hintBtn, options));
--- app/assets/javascripts/question-two.js
+++ app/assets/javascripts/question-two.js
@@ -2,13 +2,11 @@ import onSubmit from './submit-button.js'
 
 const questionCheck = document.querySelector("#question-form");
 const hintBtn = document.querySelector("#hint");
-const options = document.querySelectorAll(".govuk-checkboxes__item");
 const option1 = document.querySelector("#question-two");
 const option2 = document.querySelector("#question-two-2");
 const option3 = document.querySelector("#question-two-3");
 const option4 = document.querySelector("#question-two-4");
 const option5 = document.querySelector("#question-two-5");
-const option6 = document.querySelector("#question-two-6");
 
 // Add event listener to form submission and hint button
 questionCheck.addEventListener("submit", (event)=>onSubmit(event,isCorrectCheck));
